885b7b6d9dc372fc53649d9f9410e24cfb04ad5b
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-31 15:41:33 -0500

    html doc finished

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(function() {
+
+const topics = ["World of Warcraft", "Settlers of Catan", "Twilight Imperium", "Warhammer 40k", "Munchkin", "Super Smash Bros.", "The Legend of Zelda", "Star Fox",];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
